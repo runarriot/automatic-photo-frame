5e765b2f56f4bbf0acc63e32fa12c48a412b0028
Bartek Machocki <bmachocki@gmail.com>
2017-09-27 11:29:41 +0200

    bot working with config

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 import telebot
-import config
+import cfg
 
-TOKEN = conig.TOKEN
+TOKEN = cfg.TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['soff'])
 def screenoff(message):
-	bot.reply_to(message, "Screen off")
+        bot.reply_to(message, "Screen off")
 
 @bot.message_handler(commands=['on'])
 def screenon(message):
-	bot.reply_to(message, "Screen on")
+        bot.reply_to(message, "Screen on")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
