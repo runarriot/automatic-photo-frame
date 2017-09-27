a3a6c4a77a67e73a7597696650e211d138edb5d9
Bartek Machocki <bmachocki@gmail.com>
2017-09-27 10:40:01 +0200

    bot 1st

---
+++ scripts/bot.py
@@ -0,0 +1,17 @@
+import telebot
+
+TOKEN = '<token>'
+
+
+bot = telebot.TeleBot(TOKEN)
+bot.set_update_listener(listener) # register listener
+
+@bot.message_handler(commands=['soff'])
+def screenoff(message):
+	bot.reply_to(message, "Screen off")
+
+@bot.message_handler(commands=['on'])
+def screenon(message):
+	bot.reply_to(message, "Screen on")
+
+bot.polling()
